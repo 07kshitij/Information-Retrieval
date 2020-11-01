61b09b2ac75c9386f7653712ed724a3737360a93
kshitij <kshitijagrawal2607@gmail.com>
2020-11-01 21:09:04 +0530

    Complete Assignment 2
    TODO: Refactoring & Optimizing code

--- Assignment-2/Code/ASSIGNMENT2_17EC10063.py
+++ Assignment-2/Code/ASSIGNMENT2_17EC10063.py
@@ -31,7 +31,7 @@ InvertedPositionalIndex = OrderedDict()
 ChampionListLocal = OrderedDict()
 ChampionListGlobal = OrderedDict()
 Static_Quality_Score = []
-Leaders = []
+Leaders_List = []
 
 def sortKey(s):
     return int(s.split('.')[0])
@@ -42,9 +42,9 @@ def un_pickle():
         Static_Quality_Score = pickle.load(f)
 
     with open(LEADERS_PATH, 'rb') as f:
-        Leaders = pickle.load(f)
+        Leaders_List = pickle.load(f)
 
-    return Static_Quality_Score, Leaders
+    return Static_Quality_Score, Leaders_List
 
 def tf_idf_score():
 
@@ -73,7 +73,7 @@ def tf_idf_score():
 
         # Lemmatize and tokenize
         for token in word_tokenize(file_contents):
-            if token not in stopwords and not token.isnumeric():
+            if token not in stopwords:
                 token = lemmatizer.lemmatize(token)
                 try:
                     tf_td[(token, docId)] += 1
@@ -89,6 +89,7 @@ def tf_idf_score():
 
     for token, docId in tf_td.keys():
         idf_t[token] = np.log10(N / len(df_t[token]))
+        tf_td[(token, docId)] = np.log10(1 + tf_td[(token, docId)])
         tf_idf[(token, docId)] = tf_td[(token, docId)] * idf_t[token]
 
         try:
@@ -119,6 +120,8 @@ def tf_idf_score():
     with open('../ChampionListGlobal.json', 'w') as f:
         json.dump(ChampionListGlobal, f, indent=2)
 
+    print(len(InvertedPositionalIndex.keys()))
+
     return N
 
 def tf_idf_vector(query, docId):
@@ -130,11 +133,66 @@ def tf_idf_vector(query, docId):
             vector.append(0)
     return vector
 
-def local_championlist_vector(query):
-    return
+def local_championlist_vector(query, docId):
+    vector = []
+    for word, idf in InvertedPositionalIndex.keys():
+        if (word, docId) in tf_idf.keys():
+            vector.append(tf_idf[(word, docId)])
+        else:
+            vector.append(0)
+    return vector
 
-def global_championlist_vector(query):
-    return
+def global_championlist_vector(query, docId):
+    vector = []
+    for word, idf in InvertedPositionalIndex.keys():
+        if (word, docId) in tf_idf.keys():
+            vector.append(tf_idf[(word, docId)])
+        else:
+            vector.append(0)
+    return vector
+
+leaders_vector = {}
+norm_leader = {}
+followers_vector = {}
+Followers_List = {}
+
+def compute_followers(N):
+
+    for leader in Leaders_List:
+        vector = []
+        for word, idf in InvertedPositionalIndex.keys():
+            if (word, leader) in tf_idf.keys():
+                vector.append(tf_idf[(word, leader)])
+            else:
+                vector.append(0)
+        leaders_vector[leader] = np.array(vector)
+        norm_leader[leader] = norm(leaders_vector[leader])
+
+    for docId in range(N):
+        if docId not in Leaders_List:
+            vector = []
+            for word, idf in InvertedPositionalIndex.keys():
+                if (word, docId) in tf_idf.keys():
+                    vector.append(tf_idf[(word, docId)])
+                else:
+                    vector.append(0)
+            followers_vector[docId] = np.array(vector)
+
+            n = norm(followers_vector[docId])
+
+            if n:
+                max_score = 0; L = None
+                for leader in Leaders_List:
+                    if norm_leader[leader]:
+                        score = dot(followers_vector[docId], leaders_vector[leader])
+                        score /= n
+                        score /= norm_leader[leader]
+                        if score > max_score:
+                            max_score = score; L = leader
+                try:
+                    Followers_List[L].append(docId)
+                except KeyError:
+                    Followers_List[L] = [docId]
 
 def answer_query(query_file, N):
 
@@ -150,17 +208,13 @@ def answer_query(query_file, N):
             words = []
 
             for word in word_tokenize(query):
-                if word not in stopwords and not word.isnumeric():
+                if word not in stopwords:
                     word = lemmatizer.lemmatize(word)
                     words.append(word)
-                    # if word in idf_t.keys():
-                    #     query_vector.append(idf_t[word])
-                    # else:
-                    #     query_vector.append(0)
 
             for word, idf in InvertedPositionalIndex.keys():
                 if word in words:
-                    query_vector.append(idf_t[word])
+                    query_vector.append(idf)
                 else:
                     query_vector.append(0)
 
@@ -168,12 +222,11 @@ def answer_query(query_file, N):
 
             query_vector = np.array(query_vector)
 
-            # print(query_vector)
 
             for docId in range(N):
                 ss = np.array(tf_idf_vector(words, docId))
 
-                if norm(ss) != 0:            
+                if norm(ss) != 0 and norm(query_vector) != 0:            
                     score = dot(query_vector, ss) / (norm(query_vector) * norm(ss))
                     scores.append((docId, score))
                     # print(docId, ss, score)
@@ -182,10 +235,73 @@ def answer_query(query_file, N):
             scores = scores[:min(10, len(scores))]
             print(scores)
 
+            cache = []
+            scores = []
 
-    return
+            for word in words:
+                for docId in ChampionListLocal[word]:
+                    if docId in cache:
+                        continue
+                    cache.append(docId)
+                    ss = np.array(local_championlist_vector(words, docId))
+                    if norm(ss) != 0 and norm(query_vector) != 0:            
+                        score = dot(query_vector, ss) / (norm(query_vector) * norm(ss))
+                        scores.append((docId, score))
+
+            scores = sorted(scores, key=lambda x: x[1], reverse=True)
+            scores = scores[:min(10, len(scores))]
+            print(scores)
+
+            cache = []
+            scores = []
+
+            for word in words:
+                for docId in ChampionListGlobal[word]:
+                    if docId in cache:
+                        continue
+                    cache.append(docId)
+                    ss = np.array(global_championlist_vector(words, docId))
+                    if norm(ss) != 0 and norm(query_vector) != 0:            
+                        score = dot(query_vector, ss) / (norm(query_vector) * norm(ss))
+                        scores.append((docId, score))
+
+            scores = sorted(scores, key=lambda x: x[1], reverse=True)
+            scores = scores[:min(10, len(scores))]
+            print(scores)
+
+            n = norm(query_vector)
+            compute_followers(N)
+
+            # print(Followers_List)
+
+            scores = []
+
+            for leader in Leaders_List:
+                score = dot(query_vector, leaders_vector[leader])
+                if norm_leader[leader] and n:
+                    score /= n
+                    score /= norm_leader[leader]
+                    scores.append((leader, score))
+
+            scores = sorted(scores, key=lambda x: x[1], reverse=True)
+            L_q = scores[0][0]
+            ss = scores[0]
+
+            scores = []
+            scores.append(ss)
+
+            for follower in Followers_List[L_q]:
+                score = dot(query_vector, followers_vector[follower])
+                if norm(followers_vector[follower]) and n:
+                    score /= n
+                    score /= norm(followers_vector[follower])
+                    scores.append((follower, score))
+
+            scores = sorted(scores, key=lambda x: x[1], reverse=True)
+            scores = scores[:min(10, len(scores))]
+            print(scores)
 
 if __name__ == "__main__":
-    Static_Quality_Score, Leaders = un_pickle()
+    Static_Quality_Score, Leaders_List = un_pickle()
     N = tf_idf_score()
     answer_query('query.txt', N)
